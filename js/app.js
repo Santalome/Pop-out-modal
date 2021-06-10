0a3fbbea1c51b16c9735c3d0c860f071ec79a4bb
Mateo Gomez <mateo.gomez.santa@gmail.com>
2021-06-09 21:36:42 -0400

    Updates

--- js/app.js
+++ js/app.js
@@ -5,11 +5,13 @@ window.onload = function(){
 
     let emailModal = document.getElementsByClassName('email-modal')[0];
 
-    let closeModal = document.getElementsByClassName('email-modal__close-btn')[0];
+    let closeButton = document.getElementsByClassName('email-modal__close-btn')[0];
 
     let emailInput = document.getElementsByClassName('email-modal__input')[0];
 
     let emailButton = document.getElementsByClassName('email-modal__button')[0];
+
+    let email
 
     function emailIsValid (email){
         return /\S+@\S+\.\S+/ .test(email);
@@ -19,12 +21,15 @@ window.onload = function(){
         if(emailIsValid(emailInput.value)){
             console.log(emailInput.value);
         }else{
-            alert('This is not a valid email.');
+            document.getElementsByClassName('email-modal__form-group')[0].classList.add
+            ('email-modal__form-group--error');
+            document.getElementsByClassName('email-modal__error-message')[0].classList.add
+            ('email-modal__error-message--active');
         }
         
     })
 
-    closeModal.addEventListener('click', () =>{
+    closeButton.addEventListener('click', () =>{
         emailModal.classList.remove('email-modal--visible');
     });
 
